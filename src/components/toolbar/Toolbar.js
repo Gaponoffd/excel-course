42fdbecb5635eee24d420b02997ec6d674da60a6
gaponoffd <gaponoffd@yandex.ru>
2020-11-21 00:24:35 +0300

    Finish base table logic

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,10 +1,10 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 export class Toolbar extends ExcelComponent {
   static className = 'excel__toolbar'
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
       name: 'Toolbar',
-      listeners: ['click']
+      ...options
     })
   }
   toHtml() {
@@ -28,9 +28,5 @@ export class Toolbar extends ExcelComponent {
         <span class="material-icons">format_underlined</span>
       </div>
     `
-  }
-
-  onClick(event) {
-    console.log(event.target);
   }
 }
